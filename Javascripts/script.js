3f10725479baf90f4cad91645905248ee40d0f0b
unknown <mi.langcreations@gmail.com>
2021-02-02 06:40:14 +0530

    initial commit

---
+++ Javascripts/script.js
@@ -0,0 +1,8 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
+const main = document.querySelector('.main');
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+
+main.style.width = width + "px";
+main.style.height = height + "px";
